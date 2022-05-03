9f9e4a067907037c5de2ec8403728dc9227c9405
mcmanyika <mcmanyika.com>
2022-05-03 14:18:04 -0500

    added moralis configurations

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
 
+import { MoralisProvider } from 'react-moralis'
+import { ModalProvider } from 'react-simple-hook-modal'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER} 
+      appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}>
+      <ModalProvider>
+        <Component {...pageProps} />
+      </ModalProvider>
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
